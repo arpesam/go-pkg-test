d1b9f8b89e077fbf90a47dd2c97bb5bebe28e2a8
Danilo Sampaio <daniloarpesam@gmail.com>
2022-12-29 23:49:17 +0100

    add v2 folder

--- calc.go
+++ calc.go
@@ -1,15 +1,8 @@
 package calc
 
-import (
-	"errors"
+import "github.com/sirupsen/logrus"
 
-	"github.com/sirupsen/logrus"
-)
-
-func Sum(num1, num2 int) (int, error) {
-	if num1 == 0 || num2 == 0 {
-		return 0, errors.New("do not sum zero numbers")
-	}
+func Sum(num1, num2 int) int {
 	logrus.Infof("The numbers are - num1=%d, num2=%d", num1, num2)
-	return (num1 + num2), nil
+	return num1 + num2
 }
---
+++ v2/calc.go
@@ -0,0 +1,15 @@
+package calc
+
+import (
+	"errors"
+
+	"github.com/sirupsen/logrus"
+)
+
+func Sum(num1, num2 int) (int, error) {
+	if num1 == 0 || num2 == 0 {
+		return 0, errors.New("do not sum zero numbers")
+	}
+	logrus.Infof("The numbers are - num1=%d, num2=%d", num1, num2)
+	return (num1 + num2), nil
+}
