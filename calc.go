b283fdb038cd48303da6194c327d5263bd42b5c9
Danilo Sampaio <daniloarpesam@gmail.com>
2022-12-29 23:21:43 +0100

    update logs

--- calc.go
+++ calc.go
@@ -3,6 +3,6 @@ package calc
 import "github.com/sirupsen/logrus"
 
 func Sum(num1, num2 int) int {
-	logrus.Infof("num1=%d, num2=%d", num1, num2)
+	logrus.Infof("numbers - num1=%d, num2=%d", num1, num2)
 	return num1 + num2
 }
