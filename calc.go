c446644d427751f598611a3cd9441c8ed30babcf
Danilo Sampaio <daniloarpesam@gmail.com>
2022-12-30 15:07:00 +0100

    update sum to three params

--- calc.go
+++ calc.go
@@ -2,7 +2,7 @@ package calc
 
 import "github.com/sirupsen/logrus"
 
-func Sum(num1, num2 int) int {
+func Sum(num1, num2, num3 int) int {
 	logrus.Infof("The numbers are - num1=%d, num2=%d", num1, num2)
-	return num1 + num2
+	return num1 + num2 + num3
 }
